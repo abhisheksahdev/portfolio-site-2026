a08b1bf8e3941210c3f8f967b696b68b7ab996e4
Abhishek Sah <sah755146@gmail.com>
2026-05-27 20:21:36 +0530

    added project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex">
+      {/* navbar */}
+      <nav className="flex w-screen top-[1vw] items-center justify-center fixed">
+        <div className="absolute top-0 left-[1vw]">Logo</div>
+        <div className="font-medium">Sat 20:45, Ethelbari</div>
+        <div className="absolute top-0 right-[1vw] flex gap-[3vw]">
+          <div>Services</div>
+          <div>Portfolio</div>
+          <div>Resume</div>
+        </div>
+      </nav>
+      {/*  */}
+    </div>
+  );
 }
